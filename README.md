ac91da83a4f7f1b8554eedcf01a3c168d283119f
minquan.chen <minquan.chen@daocloud.io>
2022-05-19 22:45:32 +0800

    继续学习os包

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ the go sandard library tutorals for go SDK 1.17.9.
 总的来说，每周了解一个stander library,达到：WW等级。
 
 * 2022.4.30 开始学习 fmt
+
+## 疑问
+* os func DirFS(dir string) fs.FS
+* os func IsExist(err error) bool
--- os/file_test.go
+++ os/file_test.go
@@ -1,6 +1,7 @@
 package os_tutorial
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"testing"
@@ -30,5 +31,71 @@ func Test_file(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	//
+	//清除环境变量
+	os.Clearenv()
+
+	//将一个dir打开问一个文件目录
+	fs := os.DirFS("/Users/fraps/daocloud/github-code/go-Standard-library-tutorials/os")
+	fmt.Println(fs)
+
+	//返回所有环境变量
+	envs := os.Environ()
+	fmt.Println(envs)
+
+	//返回执行当前进程的文件
+	fmt.Println(os.Executable())
+
+	//os.Exit(1)
+
+	//字符串替代
+	mapper := func(placeholderName string) string {
+		switch placeholderName {
+		case "DAY_PART":
+			return "morning"
+		case "NAME":
+			return "Gopher"
+		}
+
+		return ""
+	}
+	fmt.Println(os.Expand("Good ${DAY_PART}, $NAME!", mapper))
+
+	//用环境变量替换
+	os.Setenv("NAME", "gopher")
+	os.Setenv("BURROW", "/usr/gopher")
+	fmt.Println(os.ExpandEnv("$NAME lives in ${BURROW}."))
+
+	//返回 有效用户组id
+	fmt.Println(os.Getegid())
+
+	//获取环境变量
+	os.Setenv("NAME", "gopher")
+	os.Setenv("BURROW", "/usr/gopher")
+	fmt.Printf("%s lives in %s.\n", os.Getenv("NAME"), os.Getenv("BURROW"))
+
+	//获取 用户id
+	fmt.Println(os.Geteuid())
+
+	//返回 用户组id
+	fmt.Println(os.Getgid())
+
+	fmt.Println(os.Getgroups())
+
+	fmt.Println(os.Getpagesize())
+
+	fmt.Println(os.Getpid())
+
+	fmt.Println(os.Getppid())
+
+	fmt.Println(os.Getuid())
+
+	fmt.Println(os.Getwd())
+
+	fmt.Println(os.Hostname())
+
+	//这个函数 比较有意思
+	//fmt.Println(os.IsExist())
+
+	//fmt.Println(os.IsNotExist())
+
 }
